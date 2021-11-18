c925bb47d152a76e2e8a663d756f2e5cf2a87ca4
Shivam Kumar Jha <shivam.jha@successive.tech>
2021-11-18 18:32:46 +0530

    Bootstrap appolo server and query

--- src/module/user/query.js
+++ src/module/user/query.js
@@ -1,9 +1,7 @@
 export default {
-  getMyProfile: () => {
-    return {
-      id: 1,
-      name: 'Shivam Jha',
-      email: 'shivam.Jha@successive.tech'
-    }
-  },
-}
+  getMyProfile: () => ({
+    id: 1,
+    name: 'Shivam Jha',
+    email: 'shivam.Jha@successive.tech',
+  }),
+};
