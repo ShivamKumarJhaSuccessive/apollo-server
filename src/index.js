bc0a10186f94e47f4f98e0e2093a4f5fba3bf733
Shivam Kumar Jha <shivam.jha@successive.tech>
2021-11-24 14:39:42 +0530

    Add Subscription

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
+/* eslint-disable no-console */
 import schema from './module';
 import Server from './server';
 import config from './config/configurations';
 
 const server = new Server(config);
-
+console.log('http://localhost:9001/graphql');
 (() => {
   server.bootstrap().setupApollo(schema);
 })();
